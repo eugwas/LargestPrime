8b909a60aaa96955ad9c4c277e2611ddeaf43f64
Eugeniusz W <eugwas@gmail.com>
2021-10-26 17:54:15 +0200

    Scanner was added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Largest prime factor = " + LargestPrime.getLargestPrime(217));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
+            int number = scanner.nextInt();
+            System.out.println("Largest prime factor = " + LargestPrime.getLargestPrime(number));
+        } else {
+            System.out.println("Invalid number");
+        }
+        scanner.nextLine();
+        scanner.close();
+
     }
 }
